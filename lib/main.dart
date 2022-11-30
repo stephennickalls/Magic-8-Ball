ba81d5a99d8a763e17b53704db8f5bce279e0a92
Stephen_Nickalls <stephennickalls@gmail.com>
2022-11-30 22:45:14 +0000

    Project complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Ask Me Anything'),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: EightBall(),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    ),
+  );
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
 
+class _EightBallState extends State<EightBall> {
+  int ballImageNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballImageNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballImageNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
